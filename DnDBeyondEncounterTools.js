54f0d4a79c81463e0df4362e762ff7a75125be13
Sowry <sowry@rascality.nz>
2024-02-05 16:46:05 +1300

    Initial commit of first release.

---
+++ DnDBeyondEncounterTools.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name         DnDBeyond Encounter Tools
+// @description  Encounter Tools
+// @version      0.2.0
+// @author       Sowry
+// @namespace    Sowry
+// @downloadURL  https://raw.githubusercontent.com/sowry-rascality/EncounterToolsDist/main/DnDBeyondEncounterTools.js
+// @updateURL    https://raw.githubusercontent.com/sowry-rascality/EncounterToolsDist/main/DnDBeyondEncounterTools.js
+// @homepageURL  https://github.com/sowry-rascality/DnDBeyondEncounterTools
+// @match        https://www.dndbeyond.com/*
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
+(() => {
+    const version = "0.2.0";
+    console.log(`Loaded Version ${version}`);
+
+    'use strict';
+
+    let scriptId = 'DnDBeyondEncounterToolsScript'; // you could encode the css path itself to generate id..
+    let styleId = 'DnDBeyondEncounterToolsStyle';
+    let head = document.getElementsByTagName('head')[0];
+    let host = 'https://raw.githubusercontent.com/sowry-rascality/EncounterToolsDist'
+
+    document.querySelectorAll('script').forEach((script) => {
+        if (script.src.includes('https://media.dndbeyond.com/encounter-builder/static/js')) {
+          script.remove();
+          // We want to be the only encounter builder script that runs.
+        }
+    })
+
+    if (!document.getElementById(scriptId)) {
+        let script = document.createElement('script');
+        script.id = scriptId;
+        script.src = `${host}/bundle.js`;
+        head.appendChild(script);
+    }
+    // if (!document.getElementById(styleId)) {
+    //     let style = document.createElement('style');
+    //     style.id = scriptId;
+    //     style.src = `${host}/bundle.css`;
+    //     head.appendChild(style);
+    // }
+})();
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# EncounterTools
-Encounter Tools for DnDBeyond
+# EncounterToolsDist
+Extension of Tools for Encounters on DnDBeyond
+
+## How To Use
+
+1. Install a userscript extension of your choice. I've tested [Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/) in Mozilla FireFox and [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo) in Google Chrome.
+2. Install the userscript from [DnDBeyondEncounterTools.js](https://raw.githubusercontent.com/sowry-rascality/EncounterToolsDist/main/DnDBeyondEncounterTools.js)
